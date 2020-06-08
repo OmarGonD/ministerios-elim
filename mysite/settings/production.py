baf7ab31e2b42e67df4ad52468e4250ceb3804e1
OmarGonD <oma.gonzales@gmail.com>
2020-06-08 15:52:51 -0500

    First Heroku commit

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,6 +1,21 @@
+from __future__ import absolute_import, unicode_literals
 from .base import *
+import dj_database_url
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
 
 DEBUG = False
+
+#Parse databse configuration from $DATABASE_URL
+DATABASES['detaul'] = dj_database_url.config()
+
+#Honor the 'X-Forwareded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWAREDED_PROTO', 'http')
+
+ALLOWED_HOSTS = ['*']
+
 
 try:
     from .local import *
