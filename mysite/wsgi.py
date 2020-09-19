303907488d37e4522d689ebdb8fc8cfaf5c92131
OmarGonD <oma.gonzales@gmail.com>
2020-09-18 19:31:05 -0500

    modified wsgi for heroku push

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,9 +1,20 @@
 import os
+import sys
 
 import dotenv
 from django.core.wsgi import get_wsgi_application
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("##########")
+print("WSGI")
+print(path)
+print("##########")
+
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
