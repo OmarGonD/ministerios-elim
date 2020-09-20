063f0e888905adfe229230e55529e2ea88ab5fde
OmarGonD <oma.gonzales@gmail.com>
2020-09-20 17:02:59 -0500

    Added AWS settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -197,7 +197,11 @@ STATICFILES_DIRS = [
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
 # Javascript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/3.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+#STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static') #original
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -226,3 +230,21 @@ WAGTAIL_ADDRESS_MAP_LANGUAGE = os.getenv('WAGTAIL_ADDRESS_MAP_LANGUAGE')
 
 
 LOGIN_REDIRECT_URL = '/'
+
+### AMAZON ###
+
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
