b08829f7a5a5ab10b429944597e8dda9b6153e10
ogonzales <oma.gonzales@gmail.com>
2025-12-28 14:07:04 -0500

    Fix iglesias app path for PythonAnywhere

--- iglesias/apps.py
+++ iglesias/apps.py
@@ -1,3 +1,4 @@
+import os
 from django.apps import AppConfig
 
 
@@ -5,3 +6,4 @@ class IglesiasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'iglesias'
     verbose_name = 'Iglesias'
+    path = os.path.dirname(os.path.abspath(__file__))
